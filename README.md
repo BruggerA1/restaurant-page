fd68a18e6bb06076df9800e5829b38654835cbd3
Brugger Anderson <BruggerA1@gmail.com>
2022-01-05 19:49:34 -0500

    update

--- README.md
+++ README.md
@@ -1,9 +1,1 @@
 #  Restaurant Page (Webpack Demo)
-
-### To Do ###
-- Home Page (Images / Content)
-- PageLoader CSS
-
-- Menu Page (Images / Item Class)
-
-- Contact Page
--- webpack.config.js
+++ webpack.config.js
@@ -9,7 +9,7 @@ const isProduction = process.env.NODE_ENV == "production";
 const stylesHandler = MiniCssExtractPlugin.loader;
 
 const config = {
-  entry: "./src/index/index.js",
+  entry: "./src/index.js",
   output: {
     path: path.resolve(__dirname, "dist"),
   },
@@ -19,7 +19,7 @@ const config = {
   },
   plugins: [
     new HtmlWebpackPlugin({
-      template: "./src/index/index.html",
+      template: "./src/index.html",
     }),
 
     new MiniCssExtractPlugin(),
@@ -39,9 +39,8 @@ const config = {
       },
       {
         test: /\.(eot|svg|ttf|woff|woff2|png|jpg|gif)$/i,
-        type: "asset",
+        type: "asset/resource",
       },
-
       // Add your rules for custom modules here
       // Learn more about loaders from https://webpack.js.org/loaders/
     ],
